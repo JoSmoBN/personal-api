5ef716c5307c841e200a771ef55b6804b578261d
Joseph Nolan <josephn0624@gmail.com>
2016-06-16 16:42:54 -0500

    all the changes

---
+++ models/me.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: 'Joseph Nolan',
+  location: 'Dallas, TX',
+  occupations: [],
+  hobbies: [{
+    "name": "Gaming",
+    "type": "relax"
+  },{
+    "name": "Guitar",
+    "type": "music",
+  },{
+    "name": "Hiking",
+    "type": "outdoor"
+  },{
+    "name": "Recording Music",
+    "type": "music",
+  },{
+    "name": "Netflixing",
+    "type": "relax",
+  },{
+    "name": "Songwriting",
+    "type": "music",
+  },{
+    "name": "Sleeping",
+    "type": "relax",
+  }],
+  skillz: [],
+  secrets: {
+    "Favorite People": "Brittney, Joos, Paul, Rachel, Lindsay, Chase and Alison"
+  }
+}
